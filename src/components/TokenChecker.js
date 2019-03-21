b92d729d7d48edb542d8ca1382cecb0414f71cdb
qed0711 <qdizon@gmail.com>
2019-03-20 20:22:15 -0400

    inital commit

---
+++ src/components/TokenChecker.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class TokenChecker extends Component {
+
+    render(){
+        return(
+            <div className="token-checker">
+                <heading>Musictheoryportal.com Token Checker</heading>
+            </div>
+        )
+        
+    }
+}
+
+
+export default TokenChecker;
